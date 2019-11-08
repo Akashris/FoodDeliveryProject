ae99f530224682aafbe1aad0631191c38859ec7d
Akashris <akashritharan@gmail.com>
2019-11-08 14:52:16 +0530

    Updated

--- FoodDeliveryProject/FoodDeliveryProject/MaximumTip.cs
+++ FoodDeliveryProject/FoodDeliveryProject/MaximumTip.cs
@@ -8,15 +8,18 @@ namespace FoodDeliveryProject
         int[] deliveryBoyTwoTips = new int[100];
         int[] difference = new int[100];
         int[] sort = new int[100];
+        int validTipFlag=0;
         int orders, tempTotalOrder, TotalOrderDeliveryBoy1, TotalOrderDeliveryBoy2, arrayTraverseVariable, sortTraverseVariable, index, arrayLength, temp = 0, maxTip = 0, orderCountForBoy1 = 0, orderCountForBoy2 = 0;
         string tipValueFor1, tipValueFor2;
 
         public void getValue()
         {
-            try
+
+
+            orders = -1;
+            while (orders < 1)
             {
-                orders = -1;
-                while (orders < 1)
+                try
                 {
                     Console.Write("\nEnter The orders: ");
                     orders = Convert.ToInt32(Console.ReadLine());
@@ -26,38 +29,194 @@ namespace FoodDeliveryProject
                     }
 
                 }
-                tempTotalOrder = orders - 1;
-                while (tempTotalOrder < orders)
+                catch (FormatException)
                 {
-                    Console.Write("\nNo. Of Orders For Delivery Boy One = ");
-                    TotalOrderDeliveryBoy1 = Convert.ToInt32(Console.ReadLine());
-                    Console.Write("\nNo. Of Orders For Delivery Boy Two = ");
-                    TotalOrderDeliveryBoy2 = Convert.ToInt32(Console.ReadLine());
-                    tempTotalOrder = TotalOrderDeliveryBoy1 + TotalOrderDeliveryBoy2;
-                    if (tempTotalOrder < orders)
+                    Console.WriteLine("\nEnter a Valid Number");
+
+                }
+            }
+
+
+            tempTotalOrder = orders - 1;
+            while (tempTotalOrder < orders)
+            {
+                //Console.Write("\nNo. Of Orders For Delivery Boy One = ");
+                //TotalOrderDeliveryBoy1 = Convert.ToInt32(Console.ReadLine());
+                TotalOrderDeliveryBoy1 = -1;
+                while (TotalOrderDeliveryBoy1 < 1)
+                {
+                    try
                     {
-                        Console.WriteLine("\nSum Of Both Delivery Boy's Orders Must Be Equal Or Greater Than total Orders");
+
+
+                        Console.Write("\nNo. Of Orders For Delivery Boy One = ");
+                        TotalOrderDeliveryBoy1 = Convert.ToInt32(Console.ReadLine());
+                        if (TotalOrderDeliveryBoy1 <= 0)
+                        {
+                            Console.WriteLine("\nEnter a Positive or Non Zero number");
+                        }
+                    }
+                    catch (FormatException)
+                    {
+                        Console.WriteLine("\nEnter a Valid Number");
+
                     }
                 }
+                // Console.Write("\nNo. Of Orders For Delivery Boy Two = ");
+                // TotalOrderDeliveryBoy2 = Convert.ToInt32(Console.ReadLine());
+                TotalOrderDeliveryBoy2 = -1;
+                while (TotalOrderDeliveryBoy2 < 1)
+                {
+                    try
+                    {
+                        Console.Write("\nNo. Of Orders For Delivery Boy Two = ");
+                        TotalOrderDeliveryBoy2 = Convert.ToInt32(Console.ReadLine());
+                        if (TotalOrderDeliveryBoy2 <= 0)
+                        {
+                            Console.WriteLine("\nEnter a Positive or Non Zero number");
+                        }
+                    }
+                    catch (FormatException)
+                    {
+                        Console.WriteLine("\nEnter a Valid Number");
+
+                    }
+
+                }
+                tempTotalOrder = TotalOrderDeliveryBoy1 + TotalOrderDeliveryBoy2;
+                if (tempTotalOrder < orders)
+                {
+                    Console.WriteLine("\nSum Of Both Delivery Boy's Orders Must Be Equal Or Greater Than total Orders");
+                }
+            }
+
+            arrayLength = orders;
+            Console.WriteLine("\nEnter The Tips For Both Delivery Boys: ");
 
 
-                Console.WriteLine("\nEnter The Tips For Both Delivery Boys: ");
+            /*for (arrayTraverseVariable = 0; arrayTraverseVariable < orders; arrayTraverseVariable++)
+            {
+                deliveryBoyOneTips[arrayTraverseVariable] = -1;
+                deliveryBoyTwoTips[arrayTraverseVariable] = -1;
+            }*/
+
+
+            while(validTipFlag == 0)
+            {
+
                 Console.Write("\nDelivery Boy One = ");
                 tipValueFor1 = Console.ReadLine();
+                for (arrayTraverseVariable = 0; arrayTraverseVariable < arrayLength; arrayTraverseVariable++)
+                {   try
+                    {
+                        try
+                        {
+                            deliveryBoyOneTips[arrayTraverseVariable] = Int32.Parse(tipValueFor1.Split(' ')[arrayTraverseVariable]);
+                            if(deliveryBoyOneTips[arrayTraverseVariable]<0)
+                            {
+                                Console.WriteLine("\nEnter a Positive Number or Zero as Tip Amount");
+                                validTipFlag = 0;
+                                break;
+                            }
+                            else
+                            {
+                                validTipFlag = 1;
+                            }
+                        }
+                        catch(IndexOutOfRangeException)
+                        {
+                            if (deliveryBoyOneTips.Length != orders)
+                            {
+                                Console.WriteLine("\nEnter {0} Tip Amount For Delivery Boy 1", orders);
+                                validTipFlag = 0;
+                                break;
+                            }
+                            /*Console.WriteLine("\nEnter {0} Tip Amount For Each Delivery Boy",orders);
+                            validTipFlag = 0;
+                            break;*/
+                        }
+                        
+                    }
+                    catch (FormatException)
+                    {
+                        Console.WriteLine("\nEnter a Valid Number");
+                        validTipFlag = 0;
+                        break;
+                    }
+                }
+                /*if(deliveryBoyOneTips.Length!=orders)
+                {
+                    Console.WriteLine("\nEnter {0} Tip Amount For Delivery Boy 1", orders);
+                    validTipFlag = 0;
+                }
+                else
+                {
+                    
+                    validTipFlag = 1;
+                }*/
+            }
+
+
+            validTipFlag = 0;
+            while(validTipFlag==0)
+            {
                 Console.Write("\nDelivery Boy Two = ");
                 tipValueFor2 = Console.ReadLine();
-                arrayLength = tipValueFor1.Split(" ").Length;
                 for (arrayTraverseVariable = 0; arrayTraverseVariable < arrayLength; arrayTraverseVariable++)
-                {
-                    deliveryBoyOneTips[arrayTraverseVariable] = Int32.Parse(tipValueFor1.Split(' ')[arrayTraverseVariable]);
-                    deliveryBoyTwoTips[arrayTraverseVariable] = Int32.Parse(tipValueFor2.Split(' ')[arrayTraverseVariable]);
+                {   try
+                    {
+                        try
+                        {
+                            deliveryBoyTwoTips[arrayTraverseVariable] = Int32.Parse(tipValueFor2.Split(' ')[arrayTraverseVariable]);
+                            if (deliveryBoyTwoTips[arrayTraverseVariable] < 0)
+                            {
+                                Console.WriteLine("\nEnter a Positive Number or Zero as Tip Amount");
+                                validTipFlag = 0;
+                                break;
+                            }
+                            else
+                            {
+                                validTipFlag = 1;
+                                
+                            }
+                        }
+                        catch (IndexOutOfRangeException)
+                        {
+                            if (deliveryBoyTwoTips.Length != orders)
+                            {
+                                Console.WriteLine("\nEnter {0} Tip Amount For Delivery Boy 2", orders);
+                                validTipFlag = 0;
+                                break;
+                            }
+
+                            /*Console.WriteLine("\nEnter {0} Tip Amount For Each Delivery Boy", orders);
+                            validTipFlag = 0;
+                            break;*/
+                        }
+                    }
+                    catch (FormatException)
+                    {
+                        Console.WriteLine("\nEnter a Valid Number");
+                        validTipFlag = 0;
+                        break;
+                    }
                 }
+                /*if ( deliveryBoyTwoTips.Length!= orders)
+                {
+                     Console.WriteLine("\nEnter {0} Tip Amount For Delivery Boy 2", orders);
+                     validTipFlag = 0;
+                }
+                else
+                {
+                   
+                    validTipFlag = 1;
+                }*/
 
             }
-            catch(FormatException)
-            {
-                Console.WriteLine("\nEnter a Valid Number");
-            }
+               
+
+
+           
         }
 
 
